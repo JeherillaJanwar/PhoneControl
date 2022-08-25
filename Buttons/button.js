9eaf9e79c405aeca95d267951ef9936915a34bf1
Ishaan Sharma <ishaan328069@gmail.com>
2022-08-25 13:36:15 -0400

    Update button.js

--- Buttons/button.js
+++ Buttons/button.js
@@ -3,12 +3,12 @@ let database;
 
 function setup(){
     var config = {
-        apiKey: "AIzaSyDVeRD0P145hETu39Ryh4HM8rvlTSj4Kos",
-        authDomain: "try2-70357.firebaseapp.com",
-        databaseURL: "https://try2-70357.firebaseio.com",
-        projectId: "try2-70357",
+        apiKey: "AIzaSyBKS71W1WbRjnDW3whpAotUT217xRcv_CY",
+        authDomain: "phonectrl-b01b8.firebaseapp.com",
+        databaseURL: "https://phonectrl.firebaseio.com",
+        projectId: "phonectrl-b01b8",
         storageBucket: "",
-        messagingSenderId: "99106327684"
+        messagingSenderId: "765246585907"
     };
 
     firebase.initializeApp(config);

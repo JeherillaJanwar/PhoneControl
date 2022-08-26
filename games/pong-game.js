82ea75bfab5f4bf48c1379419521657a4a6ebd57
Ishaan Sharma <ishaan328069@gmail.com>
2022-08-25 21:52:31 -0400

    Update pong-game.js

--- games/pong-game.js
+++ games/pong-game.js
@@ -19,12 +19,12 @@ function setup(){
     y = height-20;
 
     let config = {
-        apiKey: "AIzaSyBy9bA_kz9QJ6x-BUvEzJS-q1AhavbIX7o",
-        authDomain: "ctrl-e5cb6.firebaseapp.com",
-        projectId: "ctrl-e5cb6",
-        databaseURL: "https://ctrl-e5cb6-default-rtdb.firebaseio.com/",
+        apiKey: "AIzaSyDVeRD0P145hETu39Ryh4HM8rvlTSj4Kos",
+        authDomain: "try2-70357.firebaseapp.com",
+        databaseURL: "https://try2-70357.firebaseio.com",
+        projectId: "try2-70357",
         storageBucket: "",
-        messagingSenderId: "792254185253",
+        messagingSenderId: "99106327684" 
         };
 
     firebase.initializeApp(config);

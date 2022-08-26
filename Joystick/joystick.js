09b6b55750f508f88b433c9a2981883928d362a2
Ishaan Sharma <ishaan328069@gmail.com>
2022-08-25 21:35:35 -0400

    Update joystick.js

--- Joystick/joystick.js
+++ Joystick/joystick.js
@@ -19,12 +19,20 @@ function setup(){
     y = height/2;
     sizeC = width/5;
     var config = {
-        apiKey: "AIzaSyDVeRD0P145hETu39Ryh4HM8rvlTSj4Kos",
-        authDomain: "try2-70357.firebaseapp.com",
-        databaseURL: "https://try2-70357.firebaseio.com",
-        projectId: "try2-70357",
+        apiKey: "AIzaSyBy9bA_kz9QJ6x-BUvEzJS-q1AhavbIX7o",
+        authDomain: "ctrl-e5cb6.firebaseapp.com",
+        projectId: "ctrl-e5cb6",
+        databaseURL: "https://ctrl-35cb6.firebaseio.com",
         storageBucket: "",
-        messagingSenderId: "99106327684"
+        messagingSenderId: "792254185253",
+        
+        
+//        apiKey: "AIzaSyDVeRD0P145hETu39Ryh4HM8rvlTSj4Kos",
+ //       authDomain: "try2-70357.firebaseapp.com",
+   //     databaseURL: "https://try2-70357.firebaseio.com",
+     //   projectId: "try2-70357",
+       // storageBucket: "",
+        //messagingSenderId: "99106327684"
     };
 
 
